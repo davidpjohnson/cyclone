0adc7beba1f9f62065142b207d5f25018579ee59
davidpjohnson <kinsei.musashi@gmail.com>
2016-02-27 22:00:49 -0900

    fix stuff

--- install.sh
+++ install.sh
@@ -9,41 +9,48 @@ if [ $user == "" ]; then
     exit 1
 fi
 
+apt-get install -y vim
+apt-get install -y guake
+echo "[+] Installing..."
+echo
+cp sv /usr/bin/
+cp vp /usr/bin/
+cp vs /usr/bin/
+cp vx /usr/bin/
+cp vpx /usr/bin/
+cp vsx /usr/bin/
+cp vimrc /home/$user/.vimrc
+cp bashrc /home/$user/.bashrc
+rsync root@159.203.2.193:/home/yojimbo/bkup/projects.tar.gz /tmp/
+rsync root@159.203.2.193:/home/yojimbo/bkup/scripts.tar.gz /tmp/
+cd /tmp
+tar xvf projects.tar.gz
+tar xvf scripts.tar.gz
+mv projects /projects
+mv scripts /home/$user/scripts 
+echo
+echo
+echo "[+] Finished Installing...."
 
-install ()
-{
-    echo "[+] Installing..."
-    echo
-    cp sv /usr/bin/
-    cp vp /usr/bin/
-    cp vs /usr/bin/
-    cp vx /usr/bin/
-    cp vpx /usr/bin/
-    cp vsx /usr/bin/
-    cp vimrc /home/$user/.vimrc
-    cp bashrc /home/$user/.bashrc
-    rsync root@159.203.2.193:/home/yojimbo/bkup/projects.tar.gz /tmp/
-    rsync root@159.203.2.193:/home/yojimbo/bkup/scripts.tar.gz /tmp/
-    cd /tmp
-    tar xvf projects.tar.gz
-    tar xvf scripts.tar.gz
-    mv projects /projects
-    mv scripts /home/$user/scripts 
-    echo
-    echo
-    echo "[+] Finished Installing...."
-}
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
 
-distro=$(cut -f2 -d"(" /proc/version | cut -f1 -d"-") 
-
-
-if [ $distro == "debian" ]; then 
-    apt-get install -y vim
-    install
-elif [ $distro == "ubuntu" ]; then
-    apt-get install -y vim
-    install
-elif [ $distro == "fedora" ]; then
-    dnf install vim
-    echo "word"
-fi
