aac1c3f71e3858d532808f00ec96100f81cc3da7
root <root@localhost.localdomain>
2017-03-07 16:24:00 -0500

    fix typo and rename

--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 ##### Cyber Clone
 
 Cy-clone clones my prefered vimrc and bashrc files. It also installs 
-my prefered tools such as Guake. For a complet list of what id does 
+my prefered tools such as Guake. For a complet list of what it does 
 just look an install.sh.
 
 ###### How to use:
-To use: sudo ./install
+To use: 
+
+sudo ./installer.sh
 
 
---
+++ installer.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+distro=$(cut -f2 -d"(" /proc/version | cut -f1 -d"-") 
+os=$(uname)
+echo $os
+install ()
+{
+    echo $distro
+    echo "[+] Installing..."
+    echo
+    apt-get install -y vim
+    apt-get install -y nmap
+    apt-get install -y guake
+    apt-get install -y python-dev
+    apt-get install -y python-pip
+    aot-get install -y python-pip3
+    # pip install scapy
+    pip install paramiko
+    cp vimrc ~/.vimrc
+    cp bashrc ~/.bashrc
+    echo "[+] Finished Installing...."
+    echo 
+}
+echo "Determining Platform..."
+
+# Be sure to add the spaces in your if statements
+# For example [a == b] is bad
+# The right was [ a == b ]
+if [ $os == "Linux" ]; then
+    echo "Using Linux...."
+    echo "Determining Distro...."
+    if [ $distro == "debian" ]; then 
+        install
+    elif [ $distro == "ubuntu" ]; then
+        install
+    else
+        echo "Sorry I only use apt-get."
+        echo "dnf and yum is not supported"
+    fi
+
+elif [ $os == "Darwin" ]; then
+    echo "Using OS X...."
+    cp vimrc ~/.vimrc
+    cp bashrc ~/.bashrc
+
+else
+    echo "[!] Fatal Error"
+
+fi
