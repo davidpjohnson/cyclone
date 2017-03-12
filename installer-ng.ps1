6d878affc525b16b476fecf0d047e6c7ca83ba2d
David Johnson <david.johnson@binarydefense.com>
2017-03-12 16:21:50 -0400

    Improve installer-ng.ps1

--- installer-ng.ps1
+++ installer-ng.ps1
@@ -1,21 +1,70 @@
-# Windows Power Shell
-# If running installer-ng.ps1 fails you might need to set permissions
-# to allow scripts to run. To do that enter the following:
-# Set-ExecutionPolicy -ExecutionPolicy Unlimited
-# or 
-# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
-
-
-if ( !(Test-Path $profile)){
-    Write-Host "[+] Creating Windows PowerShell profile..."
-    New-Item -path $profile -type file –force
-    cp Microsoft.PowerShell_profile.ps1 C:\Users\$env:UserNmae\Documents\WindowsPowerShell\
+if (!(Test-Path $profile)){
+    New-Item -path $profile -typr file -force
+    cp Profiles\Microsoft.PowerShell_profile.ps1 C:\Users\$env:UserName\Documents\WindowsPowerShell\
+    Write-Host "[+] Copied profile"
 }
 else{
-    Write-Host "[+] Users PowerShell profile exists..."
+    Write-Host "Directory Exists"
     cp Profiles\Microsoft.PowerShell_profile.ps1 C:\Users\$env:UserName\Documents\WindowsPowerShell\
-    Write-Host "[+] Replacec old Microsoft.PowerShell_profile with shiny new one..."
+    Write-Host "[+] Copied profile" 
 }
-                                          
-Write-Host "[+] Enjoy your new PowerShell..."
 
+$os_type = (Get-WmiObject -Class Win32_ComputerSystem).SystemType -match ‘(x64)’
+
+if ($os_type -eq "True") {
+    Write-Host "[+] Installin 64bit tools..."
+    cd Win_Dev
+    Start-Process ./Git-2.11.0.3-64-bit.exe -Wait
+    Start-Process ./python-3.4.4.amd64.msi -Wait
+    Start-Process ./pycrypto-2.6.1.win-amd64-py3.4.exe -Wait
+    Start-Process ./pywin32-219.win-amd64-py3.4.exe -Wait
+    Start-Process ./wix310.exe -Wait
+    cd ..
+    Write-Host "[+] Finished installing 64bit tools..."
+    }
+else {
+    $os_type = (Get-WmiObject -Class Win32_ComputerSystem).SystemType -match ‘(x86)’
+
+    if ($os_type -eq "True") {
+        Write-Host "[+] Installing 32bit tools..." 
+        cd Win_Dev
+        Start-Process ./Git-2.11.0.3-32-bit.exe -Wait
+        Start-Process ./python-3.4.4.msi -Wait
+        Start-Process ./pycrypto-2.6.1.win32-py3.4.exe -Wait
+        Start-Process ./pywin32-220.win32-py3.4.exe -Wait
+        Start-Process ./wix310.exe -Wait
+        cd ..
+        Write-Host "[+] Finished Installing 32bit tools..."
+    }
+}
+
+Write-Host "[+] Enjoy your new Windows Dev environment..."
+
+
+ Write-Host " "
+Write-Host " "
+Start-Sleep -m 80
+Write-Host "@@@ @@@   @@@@@@        @@@  @@@  @@@@@@@@@@   @@@@@@@    @@@@@@   "
+Start-Sleep -m 80
+Write-Host "@@@ @@@  @@@@@@@@       @@@  @@@  @@@@@@@@@@@  @@@@@@@@  @@@@@@@@  "
+Start-Sleep -m 100
+Write-Host "@@! !@@  @@!  @@@       @@!  @@!  @@! @@! @@!  @@!  @@@  @@!  @@@  "
+Start-Sleep -m 100
+Write-Host "!@! @!!  !@!  @!@       !@!  !@!  !@! !@! !@!  !@   @!@  !@!  @!@  "
+Start-Sleep -m 120
+Write-Host " !@!@!   @!@  !@!       !!@  !!@  @!! !!@ @!@  @!@!@!@   @!@  !@!  "
+Start-Sleep -m 120
+Write-Host "  @!!!   !@!  !!!       !!!  !!!  !@!   ! !@!  !!!@!!!!  !@!  !!!  "
+Start-Sleep -m 200
+Write-Host "  !!:    !!:  !!!       !!:  !!:  !!:     !!:  !!:  !!!  !!:  !!!  "
+Start-Sleep -m 240
+Write-Host "  :!:    :!:  !:!  !!:  :!:  :!:  :!:     :!:  :!:  !:!  :!:  !:!  "
+Start-Sleep -m 280
+Write-Host "   ::    ::::: ::  ::: : ::   ::  :::     ::    :: ::::  ::::: ::  "
+Start-Sleep -m 300
+Write-Host "   :      : :  :    : :::    :     :      :    :: : ::    : :  :   "
+Start-Sleep -m 340
+Write-Host " "
+Start-Sleep -m 380
+Write-Host " "
+Start-Sleep -m 400
